1e266e5c1c7836dab4c77e54e160fb4e685c7a06
minyeong <mytokki0323@gmail.com>
2024-07-23 20:57:39 +0900

    [FEAT]: Review클래스 생성

---
+++ project/stampee/src/domain/Review.java
@@ -0,0 +1,13 @@
+package domain;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class Review {
+  private long reviewId;
+  private int rating;
+  private String contents;
+  private LocalDateTime createTime;
+  private List<Member> members;
+
+}
