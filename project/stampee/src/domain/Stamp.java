aa511874061798ea8486642186b93cdb3dac2faf
YangSoeun <soeun8636@naver.com>
2024-07-23 21:53:54 +0900

    [FEAT]: Stamp 클래스 생성

--- project/stampee/src/domain/Stamp.java
+++ project/stampee/src/domain/Stamp.java
@@ -1,4 +1,12 @@
 package domain;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 public class Stamp {
+  private long stampId;
+  private int count;
+  private LocalDateTime createTime;
+  private List<Member> members;
+  private List<Cafe> cafes;
 }
