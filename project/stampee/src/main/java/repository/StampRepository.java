6a3076788b1d791d5d743dcf4fb245497e2de97a
YangSoeun <soeun8636@naver.com>
2024-07-24 14:43:38 +0900

    [TEST]: 테스트 폴더 생성

---
+++ project/stampee/src/main/java/repository/StampRepository.java
@@ -0,0 +1,14 @@
+package repository;
+
+import static config.DBConnectionUtil.*;
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.time.LocalDate;
+
+import domain.Stamp;
+
+public class StampRepository {
+}
