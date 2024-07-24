20ef30ad86d77f769e4767c89a7f496af62ca093
Jin <rhkdghks21@naver.com>
2024-07-24 17:31:03 +0900

    [FIX]: member repo에서 미사용 객체 제거
    - close 메서드 추가
    - Member 멤버변수에 getter와 setter추가

--- project/stampee/src/main/java/domain/Member.java
+++ project/stampee/src/main/java/domain/Member.java
@@ -9,4 +9,32 @@ public class Member {
 	public long getMemberId() {
 		return memberId;
 	}
+
+	public void setMemberId(String password) {
+		this.password = password;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
 }
