55d9d3825ac4d03fcbaf7797b9ff4c51d3c3ecee
minyeong <mytokki0323@gmail.com>
2024-07-24 17:50:59 +0900

    [TEST]: define testMethod

--- project/stampee/src/test/java/repository/ReviewRepositoryTest.java
+++ project/stampee/src/test/java/repository/ReviewRepositoryTest.java
@@ -3,10 +3,35 @@ package repository;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 class ReviewRepositoryTest {
 
 	@BeforeEach
 	void setUp() {
+	}
+
+	@Test
+	void findAllReviews() {
+	}
+
+	@Test
+	void insertReview() {
+	}
+
+	@Test
+	void updateReview() {
+	}
+
+	@Test
+	void deleteReviewByReviewId() {
+	}
+
+	@Test
+	void findReviewByPhoneNumber() {
+	}
+
+	@Test
+	void countAllReviews() {
 	}
 }
