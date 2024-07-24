df412825709b3b99fcdedbb064b40e0f9d696733
YangSoeun <soeun8636@naver.com>
2024-07-24 15:53:37 +0900

    [FEAT]: Cafe, Member 필드 수정
    - Member: role 삭제
    - Cafe: 전화번호, 이메일, 비밀번호 추가

--- project/stampee/src/main/java/domain/Stamp.java
+++ project/stampee/src/main/java/domain/Stamp.java
@@ -8,4 +8,20 @@ public class Stamp {
 	private LocalDate createTime;
 	private Member member;
 	private Cafe cafe;
+
+	public int getCount() {
+		return count;
+	}
+
+	public LocalDate getCreateTime() {
+		return createTime;
+	}
+
+	public Member getMember() {
+		return member;
+	}
+
+	public Cafe getCafe() {
+		return cafe;
+	}
 }
