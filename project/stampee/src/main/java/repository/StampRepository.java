b6447ca47428cb535044d8863dfa9b8ca143fdc6
YangSoeun <soeun8636@naver.com>
2024-07-24 15:52:00 +0900

    [FEAT]: Stamp 저장하는 save 메소드 구현

--- project/stampee/src/main/java/repository/StampRepository.java
+++ project/stampee/src/main/java/repository/StampRepository.java
@@ -5,15 +5,21 @@ import static config.DBConnectionUtil.*;
 import java.sql.Connection;
 import java.sql.Date;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.time.LocalDate;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import domain.Stamp;
 
+
 public class StampRepository {
-	public Stamp save(long memberId, long cafeId, int count){
+	private static final Logger log = LoggerFactory.getLogger(StampRepository.class);
+
+	public Stamp save(Stamp stamp){
 		String sql = "insert into stamp(count, create_time, member_id, cafe_id) "
-			+ "values(?, ?, ?, ?, ?)";
+			+ "values(?, ?, ?, ?)";
 
 		Connection con = null;
 		PreparedStatement pstmt = null;
@@ -22,21 +28,42 @@ public class StampRepository {
 			con = getConnection();
 
 			pstmt = con.prepareStatement(sql);
-			pstmt.setInt(1, count);
-			pstmt.setDate(2, Date.valueOf(LocalDate.now()));
+			pstmt.setInt(1, stamp.getCount());
+			pstmt.setDate(2, Date.valueOf(stamp.getCreateTime()));
+			pstmt.setLong(3, stamp.getMember().getMemberId());
+			pstmt.setLong(4, stamp.getCafe().getCafeId());
+
+			pstmt.executeUpdate();
+			return stamp;
 		} catch (SQLException e) {
 			throw new RuntimeException(e);
+		} finally {
+			close(con, pstmt, null);
 		}
-		return null;
 	}
 
-	public Stamp update(Stamp stamp){
-		String sql = "";
+	private void close(Connection con, PreparedStatement pstmt, ResultSet rs) {
+		if (rs != null) {
+			try {
+				rs.close();
+			} catch (SQLException e) {
+				log.info("rs close error", e);
+			}
+		}
+		if (pstmt != null) {
+			try {
+				pstmt.close();
+			} catch (SQLException e) {
+				log.info("pstmt close error", e);
+			}
+		}
 
-		Connection con = null;
-
-		con = getConnection();
-
-		return null;
+		if (con != null) {
+			try {
+				con.close();
+			} catch (SQLException e) {
+				log.info("con close error", e);
+			}
+		}
 	}
 }
