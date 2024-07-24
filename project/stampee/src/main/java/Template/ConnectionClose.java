c40d0c7ee98409fec06a36ec75ae87a88575b589
YangSoeun <soeun8636@naver.com>
2024-07-24 18:43:13 +0900

    [FEAT]: close 메서드 template으로 분리해서 재사용 할 수 있도록 구현

---
+++ project/stampee/src/main/java/Template/ConnectionClose.java
@@ -0,0 +1,54 @@
+package Template;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public abstract class ConnectionClose {
+	private static final Logger log = LoggerFactory.getLogger(ConnectionClose.class);
+
+	public static void close(Connection con, PreparedStatement pstmt, ResultSet rs) {
+		if (rs != null) {
+			try {
+				rs.close();
+			} catch (SQLException e) {
+				log.info("rs close error", e);
+			}
+		}
+		if (pstmt != null) {
+			try {
+				pstmt.close();
+			} catch (SQLException e) {
+				// log.info("pstmt close error", e);
+			}
+		}
+		if (con != null) {
+			try {
+				con.close();
+			} catch (SQLException e) {
+				log.info("con close error", e);
+			}
+		}
+	}
+
+	public static void close(Connection con, PreparedStatement pstmt) {
+		if (pstmt != null) {
+			try {
+				pstmt.close();
+			} catch (SQLException e) {
+				// log.info("pstmt close error", e);
+			}
+		}
+		if (con != null) {
+			try {
+				con.close();
+			} catch (SQLException e) {
+				log.info("con close error", e);
+			}
+		}
+	}
+}
--- project/stampee/src/main/java/repository/StampRepository.java
+++ project/stampee/src/main/java/repository/StampRepository.java
@@ -1,22 +1,17 @@
 package repository;
 
+import static Template.ConnectionClose.*;
 import static config.DBConnectionUtil.*;
 
 import java.sql.Connection;
 import java.sql.Date;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import domain.Stamp;
 
 
 public class StampRepository {
-	private static final Logger log = LoggerFactory.getLogger(StampRepository.class);
-
 	public Stamp save(Stamp stamp){
 		String sql = "insert into stamp(stamp_id, count, create_time, member_id, cafe_id) "
 			+ "values(STAMP_SEQ.NEXTVAL, ?, ?, ?, ?)";
@@ -38,31 +33,7 @@ public class StampRepository {
 		} catch (SQLException e) {
 			throw new RuntimeException(e);
 		} finally {
-			close(con, pstmt, null);
-		}
-	}
-
-	private void close(Connection con, PreparedStatement pstmt, ResultSet rs) {
-		if (rs != null) {
-			try {
-				rs.close();
-			} catch (SQLException e) {
-				log.info("rs close error", e);
-			}
-		}
-		if (pstmt != null) {
-			try {
-				pstmt.close();
-			} catch (SQLException e) {
-				log.info("pstmt close error", e);
-			}
-		}
-		if (con != null) {
-			try {
-				con.close();
-			} catch (SQLException e) {
-				log.info("con close error", e);
-			}
+			close(con, pstmt);
 		}
 	}
 }
