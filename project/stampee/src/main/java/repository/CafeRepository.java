675fcf9408bee8c1261aa6b3047cc025b1ced03a
seojung <6dreamhigh@naver.com>
2024-07-24 18:19:19 +0900

    [FEAT] 카페점주 회원가입 기능 구현

--- project/stampee/src/main/java/repository/CafeRepository.java
+++ project/stampee/src/main/java/repository/CafeRepository.java
@@ -6,33 +6,55 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
-import domain.Cafe;
-
 public class CafeRepository {
 
-	public void userSignUp(long cafeId, String name, String address, String password, String email, String contact){
-		//sql
-		String sql = "insert into cafe(cafe_Id, name, address, password, email, contact) values(?,?,?,?,?,?) " ;
+	public void userSignUp(long cafeId, String name, String address, String password, String email, String contact) {
+		// SQL
+		String sql = "INSERT INTO cafe(cafe_Id, name, address, password, email, contact) VALUES(CAFE_SEQ.NEXTVAL,?,?,?,?,?)";
+
+		Connection conn = null;
+		PreparedStatement pstmt = null;
 
 		try {
-			//db connection
-			Connection conn = getConnection();
+			// DB connection
+			conn = getConnection();
 			conn.setAutoCommit(false);
 
-			Cafe cafe = new Cafe();
-			PreparedStatement pstmt = conn.prepareStatement(sql);
-			pstmt.setLong(1, cafeId);
-			pstmt.setString(2, name);
-			pstmt.setString(3, address);
-			pstmt.setString(4, password);
-			pstmt.setString(5, email);
-			pstmt.setString(6, contact);
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, name);
+			pstmt.setString(2, address);
+			pstmt.setString(3, password);
+			pstmt.setString(4, email);
+			pstmt.setString(5, contact);
 			pstmt.executeUpdate();
-			conn.close();
+			conn.commit();
 
 		} catch (SQLException e) {
+			if (conn != null) {
+				try {
+					conn.rollback();
+				} catch (SQLException rollbackEx) {
+					throw new RuntimeException(rollbackEx);
+				}
+			}
 			throw new RuntimeException(e);
+		} finally {
+			// Close PreparedStatement
+			if (pstmt != null) {
+				try {
+					pstmt.close();
+				} catch (SQLException e) {
+					throw new RuntimeException(e);
+				}
+			}
+			// Close Connection
+			if (conn != null) {
+				try {
+					conn.close();
+				} catch (SQLException e) {
+					throw new RuntimeException(e);
+				}
+			}
 		}
-
 	}
 }
