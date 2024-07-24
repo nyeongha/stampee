7a3dde45a79c947231e1289bda94069c71b71d05
Jin <rhkdghks21@naver.com>
2024-07-24 10:12:23 +0900

    [FIX]: Member 클래스에 role속성 추가

--- project/stampee/src/domain/Member.java
+++ project/stampee/src/domain/Member.java
@@ -1,9 +1,9 @@
 package domain;
 
 public class Member {
-  private long memberId;
-  private String password;
-  private String email;
-  private String phoneNumber;
-
+	private long memberId;
+	private String password;
+	private String email;
+	private String phoneNumber;
+	private int role;
 }
