f4c37aad6708cb0a1dd971781a9307040c1a9ba0
Jin <rhkdghks21@naver.com>
2024-07-25 12:07:31 +0900

    [FIX]: .gi ignore가 반영되지 않는 문제 수정

--- project/stampee/src/main/java/repository/MemberRepository.java
+++ project/stampee/src/main/java/repository/MemberRepository.java
@@ -11,32 +11,37 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import domain.Member;
+import dto.Authentication;
 
 public class MemberRepository {
 	private static final Logger log = LoggerFactory.getLogger(MemberRepository.class);
 
 	public Member userSignUp(Member member) {
 		// connection 영역
-		Connection conn = getConnection();
-		PreparedStatement pstmt = null;
-		try {
-			String sql =
-				"INSERT INTO member(member_id, user_name, email, password, phone_number)" +
-					"values(MEMBER_SEQ.NEXTVAL,?,?,?,?)";
+		Authentication auth = new Authentication(member);
+		if (auth.isValid()) {
+			Connection conn = getConnection();
+			PreparedStatement pstmt = null;
+			try {
+				String sql =
+					"INSERT INTO member(member_id, user_name, email, password, phone_number)" +
+						"values(MEMBER_SEQ.NEXTVAL,?,?,?,?)";
 
-			pstmt = conn.prepareStatement(sql);
-			pstmt.setString(1, member.getPassword());
-			pstmt.setString(2, member.getUserName());
-			pstmt.setString(3, member.getEmail());
-			pstmt.setString(4, member.getPhoneNumber());
-			pstmt.executeUpdate();
+				pstmt = conn.prepareStatement(sql);
+				pstmt.setString(1, member.getPassword());
+				pstmt.setString(2, member.getUserName());
+				pstmt.setString(3, member.getEmail());
+				pstmt.setString(4, member.getPhoneNumber());
+				pstmt.executeUpdate();
 
-		} catch (SQLException e) {
-			throw new RuntimeException(e);
-		} finally {
-			close(conn, pstmt);
+			} catch (SQLException e) {
+				throw new RuntimeException(e);
+			} finally {
+				close(conn, pstmt);
+			}
+			return member;
 		}
-		return member;
+
 	}
 
 }
