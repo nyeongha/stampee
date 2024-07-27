091f22a2c60f4a2809c9506a07dc0995e6fe6cf3
YangSoeun <soeun8636@naver.com>
2024-07-27 15:51:32 +0900

    [FEAT]: 메일에 전송될 내용 관리하는 enum 클래스 구현

---
+++ project/stampee/src/main/java/formatter/MailMessageFormatter.java
@@ -0,0 +1,17 @@
+package formatter;
+
+public enum MailMessageFormatter {
+	SEND_STAMP_MESSAGE("스탬프 %d개가 %s님에게 공유 되었습니다."),
+	RECEIVE_STAMP_MESSAGE("%s님으로 부터 %d개를 공유 받았습니다."),
+	FAIL_SEND_STAMP_MESSAGE("스탬프 공유에 실패했습니다."),
+	SHARE_STAMP_TITLE("카페 스탬프 공유 안내");
+	private final String message;
+
+	MailMessageFormatter(String message) {
+		this.message = message;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+}
