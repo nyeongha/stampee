8bc2d0f0aa978c71b571ccb877085e57a2569299
Jin <rhkdghks21@naver.com>
2024-07-28 14:21:58 +0900

    [FIX]: 패스워드 유효성검사규칙 수정 - 영문대문자, 소문자, 특수기호 1개 이상 포함 - 길이는 최소5글자에서 최대 20글자

--- project/stampee/src/main/java/config/ValidationRules.java
+++ project/stampee/src/main/java/config/ValidationRules.java
@@ -3,6 +3,6 @@ package config;
 public abstract class ValidationRules {
 	public final static String EMAIL_VALDIDATION_RULE = "^[a-z][a-z0-9-_.]+@[a-z0-9-_]+(.[a-z0-9-_]+){1,2}$";
 	public final static String USERNAME_VALDIDATION_RULE = "^([가-힣]{2,4}|[A-Za-z]{2,30})$";
-	public final static String PASSWORD_VALDIDATION_RULE = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\W).{10,20}$";
+	public final static String PASSWORD_VALDIDATION_RULE = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\W).{5,20}$";
 	public final static String PHONE_NUMBER_VALDIDATION_RULE = "^010-[0-9]{3,4}-[0-9]{4}$";
 }
