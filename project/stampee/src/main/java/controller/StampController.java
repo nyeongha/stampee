2d87041f1fe2e49295a4ae2099d2b3e8ce19384b
YangSoeun <soeun8636@naver.com>
2024-07-29 13:39:29 +0900

    [FEAT]: StampController 구현

--- project/stampee/src/main/java/controller/StampController.java
+++ project/stampee/src/main/java/controller/StampController.java
@@ -2,6 +2,7 @@ package controller;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 
 import dto.response.MyStampDto;
@@ -12,20 +13,36 @@ import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
+import repository.CafeRepository;
+import repository.CouponRepository;
 import repository.MemberRepository;
 import repository.StampRepository;
+import service.CafeService;
+import service.CouponService;
 import service.MailService;
 import service.StampService;
 
 public class StampController implements Initializable {
+	private static CouponService couponService;
 	private static StampService stampService;
-	private static MailService mailService = new MailService();
-	private static MemberRepository memberRepository = new MemberRepository();
-	private static StampRepository stampRepository = new StampRepository();
+	private static CafeService cafeService;
 
 	@FXML private ImageView stamp1, stamp2, stamp3, stamp4, stamp5, stamp6, stamp7, stamp8, stamp9, stamp10;
-	@FXML Label cafeName, cafeAddress;
+	@FXML private Label cafeName, cafeAddress, couponCount;;
+	@FXML private Label signature1, signature2;
 	@FXML private Pane mapContainer;
+
+	public StampController() {
+		MailService mailService = new MailService();
+		MemberRepository memberRepository = new MemberRepository();
+		StampRepository stampRepository = new StampRepository();
+		CouponRepository couponRepository = new CouponRepository();
+		CafeRepository cafeRepository = new CafeRepository();
+
+		stampService = new StampService(stampRepository, memberRepository, mailService);
+		couponService = new CouponService(couponRepository, mailService);
+		cafeService = new CafeService(cafeRepository);
+	}
 
 	@Override
 	public void initialize(URL url, ResourceBundle resourceBundle) {
@@ -38,13 +55,24 @@ public class StampController implements Initializable {
 	}
 
 	public void initData(long memberId, long cafeId){
-		stampService = new StampService(stampRepository, memberRepository, mailService);
 		MyStampDto myStamp = stampService.findMyStamp(memberId, cafeId);
-
+		int count = couponService.getMyCount(memberId, cafeId);
 		setStampImage(myStamp.getCount());
 
 		cafeName.setText(myStamp.getCafeName());
 		cafeAddress.setText(myStamp.getCafeAddr());
+		couponCount.setText(myStamp.getCafeName() + " 쿠폰 " + count + "장");
+
+		setSignatureMenu(cafeId);
+	}
+
+	private void setSignatureMenu(long cafeId) {
+		List<String> menus = cafeService.getSignatureMenu(cafeId);
+		Label[] signatureMenus = {signature1, signature2};
+
+		for(int i = 0; i<menus.size(); i++){
+			signatureMenus[i].setText(menus.get(i));
+		}
 	}
 
 	private void setStampImage(int count) {
