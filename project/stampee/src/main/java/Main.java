f049d7e05bc9d9de1402f04bcbef145b003623f0
Sohottoday <human9062@naver.com>
2024-07-30 21:44:03 +0900

    [FEAT] : 적립 요청 기능 추가

--- project/stampee/src/main/java/Main.java
+++ project/stampee/src/main/java/Main.java
@@ -1,14 +1,18 @@
+<<<<<<< HEAD
 
 import java.io.IOException;
 import java.util.List;
 import java.io.InputStream;
 
+=======
+>>>>>>> c24c581 ([FEAT] : 적립 요청 기능 추가)
 import controller.CouponController;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+<<<<<<< HEAD
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.BorderPane;
@@ -18,12 +22,15 @@ import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 
 import javafx.scene.text.Font;
+=======
+>>>>>>> c24c581 ([FEAT] : 적립 요청 기능 추가)
 import javafx.stage.Stage;
 
 
 public class Main extends Application {
 
 	@Override
+<<<<<<< HEAD
 	public void start(Stage primaryStage) throws Exception {
 		try {
 			InputStream is = getClass().getResourceAsStream("/font/Jua-Regular.ttf");
@@ -55,10 +62,27 @@ public class Main extends Application {
 		Parent root = loader.load();
 		Scene scene = new Scene(root);
 		primaryStage.setTitle("Review Block Example");
+=======
+	public void start(Stage primaryStage) throws Exception{
+		FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/KeypadView.fxml"));
+		Parent root = loader.load();
+		primaryStage.setTitle("Stamp Keypad");
+		Scene scene = new Scene(root, 600, 800);
+>>>>>>> c24c581 ([FEAT] : 적립 요청 기능 추가)
 		primaryStage.setScene(scene);
 		primaryStage.show();
 
 	}
+
+		// FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/CouponPage.fxml"));
+		// Parent root = loader.load();
+		//
+		// CouponController controller = loader.getController();
+		// controller.initData(37L); // 예시로 memberId 1을 사용
+		//
+		// primaryStage.setTitle("Coupon Viewer");
+		// primaryStage.setScene(new Scene(root));
+		// primaryStage.show();
 
 	public static void main(String[] args) {
 		launch(args);
