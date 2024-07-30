0719d78444a075330b465a5ec89424872e550d89
Sohottoday <human9062@naver.com>
2024-07-30 18:43:08 +0900

    [FIX] : 글꼴 수정

--- project/stampee/src/main/java/Main.java
+++ project/stampee/src/main/java/Main.java
@@ -1,64 +1,44 @@
 
-import java.util.List;
+import java.io.InputStream;
 
 import controller.CouponController;
-import controller.StampController;
-import domain.Review;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Rectangle;
+
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
-import repository.CafeRepository;
-import repository.MemberRepository;
-import repository.ReviewRepository;
-import service.ReviewService;
+
 
 public class Main extends Application {
 
-	// @Override
-	// public void start(Stage primaryStage) throws Exception {
-	// 	Parent root = FXMLLoader.load(
-	// 		getClass().getResource("/templates/account/LoginPage.fxml"));
-	// 	primaryStage.setTitle("카페 위치 정보");
-	// 	primaryStage.setScene(new Scene(root, 600, 800));
-	// 	primaryStage.show();
-	// }
-	//
-	// public static void main(String[] args) {
-	// 	launch(args);
-	// }
-
-	// public void start(Stage primaryStage) throws Exception {
-	// 	FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/CouponPage.fxml"));
-	// 	Parent root = loader.load();
-	//
-	// 	CouponController controller = loader.getController();
-	// 	controller.initData(39L); // 예시로 memberId 1을 사용
-	//
-	// 	primaryStage.setTitle("Coupon Viewer");
-	// 	primaryStage.setScene(new Scene(root));
-	// 	primaryStage.show();
-	// }
-	//
-	// public static void main(String[] args) {
-	// 	launch(args);
-	// }
-
+	@Override
 	public void start(Stage primaryStage) throws Exception {
-		FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/stamp.fxml"));
+		try {
+			InputStream is = getClass().getResourceAsStream("/font/Jua-Regular.ttf");
+			if (is != null) {
+				Font font = Font.loadFont(is, 12);
+				if (font != null) {
+					System.out.println("Font loaded: " + font.getName());
+				} else {
+					System.out.println("Failed to load font");
+				}
+			} else {
+				System.out.println("Font file not found");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/CouponPage.fxml"));
 		Parent root = loader.load();
 
-		StampController controller = loader.getController();
-		controller.initData(39L, 1L); // 예시로 memberId 1을 사용
+		CouponController controller = loader.getController();
+		controller.initData(37L); // 예시로 memberId 1을 사용
 
-		primaryStage.setTitle("Stamp Viewer");
+		primaryStage.setTitle("Coupon Viewer");
 		primaryStage.setScene(new Scene(root));
 		primaryStage.show();
 	}
@@ -66,23 +46,6 @@ public class Main extends Application {
 	public static void main(String[] args) {
 		launch(args);
 	}
-	//
-	// @Override
-	// public void start(Stage primaryStage) {
-	// 	try {
-	// 		FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/reviewListView.fxml"));
-	// 		Parent root = loader.load();
-	// 		Scene scene = new Scene(root);
-	// 		primaryStage.setTitle("Review Block Example");
-	// 		primaryStage.setScene(scene);
-	// 		primaryStage.show();
-	// 	} catch (Exception e) {
-	// 		e.printStackTrace();
-	// 	}
-	// }
-	//
-	// public static void main(String[] args) {
-	// 	launch(args);
-	// }
+
 }
 
