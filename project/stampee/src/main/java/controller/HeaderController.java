a8635f80290a359928d4548669c653f6a763a9ea
YangSoeun <soeun8636@naver.com>
2024-07-30 21:43:41 +0900

    [FEAT]: main 안쓰는 부분 수정

--- project/stampee/src/main/java/controller/HeaderController.java
+++ project/stampee/src/main/java/controller/HeaderController.java
@@ -29,14 +29,11 @@ public class HeaderController {
 
 	private void navigateTo(String fxmlPath, MouseEvent event) {
 		try {
-			// FXMLLoader를 사용하여 FXML 파일을 로드합니다.
 			FXMLLoader loader = new FXMLLoader(getClass().getResource(fxmlPath));
 			Parent root = loader.load();
 
-			// 이벤트 소스 (Label)에서 현재 스테이지를 가져옵니다.
-			Stage stage = (Stage) ((Label) event.getSource()).getScene().getWindow();
+			Stage stage = (Stage)((Label)event.getSource()).getScene().getWindow();
 
-			// 새로운 씬을 생성하고, 현재 스테이지에 설정합니다.
 			Scene scene = new Scene(root);
 			stage.setScene(scene);
 			stage.show();
