d0b5518ac28246216d8b8ffa0369baf68a2475cb
YangSoeun <soeun8636@naver.com>
2024-08-03 15:54:04 +0900

    [FEAT]: main 수정

--- project/stampee/src/main/java/Main.java
+++ project/stampee/src/main/java/Main.java
@@ -12,8 +12,6 @@ import service.MailService;
 import service.Scheduler;
 
 public class Main extends Application {
-
-
 	@Override
 	public void start(Stage primaryStage) {
 		try {
@@ -32,82 +30,6 @@ public class Main extends Application {
 		}
 	}
 
-
-	// public void start(Stage primaryStage) throws Exception {
-	// 	FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/stamp.fxml"));
-	// 	Parent root = loader.load();
-	//
-	// 	StampController controller = loader.getController();
-	//
-	// 	controller.initData(1L,10L); // 예시로 memberId 1을 사용
-	//
-	//
-	// 	primaryStage.setTitle("Stamp Viewer");
-	// 	primaryStage.setScene(new Scene(root));
-	//
-	// 	primaryStage.setTitle("Coupon Application");
-	// 	primaryStage.show();
-	//
-	// }
-
-	//
-	// @Override
-	// public void start(Stage primaryStage) throws Exception{
-	// 	FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/keypadView.fxml"));
-	// 	Parent root = loader.load();
-	// 	Scene scene = new Scene(root, 600, 800);
-	// 	primaryStage.setScene(scene);
-	// 	primaryStage.show();
-	// }
-	// public void start(Stage primaryStage) throws Exception {
-	// 	FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/couponPage.fxml"));
-	// 	Parent root = loader.load();
-	//
-	// 	CouponController controller = loader.getController();
-	// 	controller.initData(37L); // 예시로 memberId 1을 사용
-	//
-	// 	primaryStage.setTitle("Coupon Viewer");
-	// 	primaryStage.setScene(new Scene(root));
-	// 	primaryStage.show();
-	//
-	// }
-
-
-	// @Override
-	// public void start(Stage primaryStage) {
-	// 	try {
-	// 		FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/stamp.fxml"));
-	// 		Pane root = loader.load();
-	//
-	// 		// Initialize data if necessary
-	// 		StampController controller = loader.getController();
-	// 		controller.initData(39L, 1L);  // Example memberId and cafeId, replace with actual values
-	//
-	// 		Scene scene = new Scene(root);
-	// 		primaryStage.setScene(scene);
-	// 		primaryStage.setTitle("Stamp Management");
-	// 		primaryStage.show();
-	// 	} catch (Exception e) {
-	// 		e.printStackTrace();
-	// 	}
-	// }
-
-	// @Override
-	// public void start(Stage primaryStage) throws Exception {
-	// 	FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/createReview.fxml"));
-	// 	Parent root = loader.load();
-	//
-	// 	// CreateReviewController의 인스턴스 가져오기
-	// 	CreateReviewController controller = loader.getController();
-	//
-	// 	// initData 메서드를 호출하여 memberId와 cafeId 초기화
-	// 	controller.initData(9L, 2L); // 예시로 memberId 9와 cafeId 2 사용
-	//
-	// 	primaryStage.setTitle("Review Application");
-	// 	primaryStage.setScene(new Scene(root));
-	// 	primaryStage.show();
-	// }
-
 	public static void main(String[] args) throws MessagingException {
 		CouponRepository couponRepository = new CouponRepository();
 		MailService mailService = new MailService();
@@ -121,6 +43,7 @@ public class Main extends Application {
 				throw new RuntimeException(e);
 			}
 		});
+
 		launch(args);
 	}
 }
